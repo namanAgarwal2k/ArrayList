4f2e016fa7e9b97d3919ec84e50cecf90eb68e21
Naman Agarwal <66342870+naman2807@users.noreply.github.com>
2021-05-16 07:01:33 +0530

    Improved modifyItem()

--- src/GroceryList/GroceryList.java
+++ src/GroceryList/GroceryList.java
@@ -19,14 +19,13 @@ groceryList.forEach(System.out::println );  //isma number kase add kara list ma.
 }
 
     public void searchItemInList(String searchItem) {
-//    int position = findGroceryItem(searchItem);
-//        if(position >=0){
-//            String item = groceryList.get(position);
-//            System.out.println("found"+item+"in our grocery list");
-//        }else {
-//            System.out.println("Item"+searchItem+"is not in our grocery list");
-//        }
-        groceryList.stream().filter(item -> item.equals(searchItem)).forEach(System.out::println);
+    int position = findGroceryItem(searchItem);
+        if(position >=0){
+            String item = groceryList.get(position);
+            System.out.println("found"+item+"in our grocery list");
+        }else {
+            System.out.println("Item"+searchItem+"is not in our grocery list");
+        }
 
     }
 
@@ -35,20 +34,10 @@ groceryList.forEach(System.out::println );  //isma number kase add kara list ma.
     }
 
     public void modifyItem(String oldItem, String newItem) {
-        int position = findGroceryItem(oldItem);
-
-        if (position >= 0) {
-            String currItem = groceryList.get(position);
-            modifyGroceryItem(position, newItem);
-            System.out.println("Grocery item " + (position + 1)
-                    + " has been modified from " + currItem
-                    + " to " + groceryList.get(position)
-            );
-        } else {
-            System.out.println(oldItem + " doesn't exist.");
-        }
-
-    }
+       groceryList.stream().filter(item -> item.equals(oldItem)).forEach( e ->
+                     groceryList.set(groceryList.indexOf(e), newItem));
+                     
+ }
 
     private void modifyGroceryItem(int position, String newItem) {
     groceryList.set(position,newItem);
